79e3c79b92e1adece3b8f0b50013b24546dee0b6
Yury Fedoseev <yfedoseev@gmail.com>
2026-04-24 23:49:02 -0700

    chore: remove diagnostic/throwaway bin targets added during L4 cache investigation

--- src/bin/extract_page.rs
+++
@@ -1,9 +0,0 @@
-use pdf_oxide::PdfDocument;
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let path = &args[1];
-    let page: usize = args[2].parse().unwrap();
-    let doc = PdfDocument::open(path).unwrap();
-    let text = doc.extract_text(page).unwrap_or_default();
-    print!("{}", text);
-}
